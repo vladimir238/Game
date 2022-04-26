0d87a6ac56ad7f060ee12442bbe8218f22ce05dd
vladimir238 <bokovvladimir1@mail.ru>
2022-04-26 05:36:52 +0300

    commit 1

---
+++ src/Merchant.java
@@ -0,0 +1,17 @@
+public class Merchant implements Seller {
+    @Override
+    public String sell(Goods goods) {
+        String result = "";
+        if (goods == Goods.POTION) {
+            result = "potion";
+        }
+        return result;
+    }
+
+
+
+    public enum Goods {
+        POTION
+
+    }
+}
---
+++ src/Seller.java
@@ -0,0 +1,4 @@
+public interface Seller {
+    String sell(Merchant.Goods goods);
+
+}

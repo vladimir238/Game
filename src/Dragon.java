679fa6609eaf62371f03356c72a5f5675b6a4a54
vladimir238 <bokovvladimir1@mail.ru>
2022-05-02 08:34:42 +0300

    Commit 2

---
+++ src/Dragon.java
@@ -0,0 +1,6 @@
+public class Dragon extends FantasyCharacter{
+
+    public Dragon(String name, int healthPoints, int strength, int dexterity, int xp, int gold) {
+        super(name, healthPoints, strength, dexterity, xp, gold);
+    }
+}
